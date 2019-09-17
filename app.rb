3b9baea14d63fa2cf80a45e76821f4c0ae898960
karamalesa1980 <karamalesa@gmail.com>
2019-09-17 21:59:48 +0300

    add instance method

--- app.rb
+++ app.rb
@@ -1,7 +1,30 @@
 class Logger
+
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end  
+
+  @@x = nil
+ 
+
   def self.sey_mi
     puts  "Hi Yuriy!"
   end  
+
+  def log_mi wat
+    @f.puts wat
+  end
+
+  private
+
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+
+    return @@x 
+  end
 end  
 
 Logger.sey_mi
+Logger.instance.log_mi 'Hi!'
